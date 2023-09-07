8713e7fce0798b659b3e7bbe35bd48fbd729e5de
Ben <pot.94.handig@gmail.com>
2023-09-07 23:45:10 +0800

    issue #7 - added conditional queries and daily clearing of processedIdStorage

--- src/controllers/SAPEventListener.ts
+++ src/controllers/SAPEventListener.ts
@@ -26,6 +26,7 @@ export class SAPEventListener {
             if (this.monitoringTime !== (new Date()).setHours(0,0,0,0)) {
                 this.monitoringTime = (new Date()).setHours(0,0,0,0);
                 SAPEventListener.processedIds.length = 0;
+                await SAPEventListener.processedIdStorage.set([]);
             }
             await ((livePool: sql.ConnectionPool, sapEventListener: SAPEventListener): Promise<void> => {
                 const fetchedIds: EventId[] = [];
@@ -36,9 +37,9 @@ export class SAPEventListener {
                                 pool.query(`
                                     SELECT
                                         TP.U_BookingNumber AS id,
-                                        CONCAT('AP-', FORMAT(head.UpdateDate, 'yyyyMMdd'), head.UpdateTS) AS serial
+                                        CONCAT('AP', head.DocNum, '-', FORMAT(head.UpdateDate, 'yyyyMMdd'), head.UpdateTS) AS serial
                                     FROM (
-                                    SELECT DocEntry, U_PVNo, UpdateDate, UpdateTS
+                                    SELECT DocEntry, DocNum, U_PVNo, UpdateDate, UpdateTS
                                     FROM OPCH WITH(NOLOCK)
                                     WHERE CAST(CreateDate AS date) = CAST(GETDATE() AS date)
                                     OR CAST(UpdateDate AS date) = CAST(GETDATE() AS date)
@@ -67,9 +68,9 @@ export class SAPEventListener {
                                     
                                     SELECT
                                         line.ItemCode AS id,
-                                        CONCAT('SO-', FORMAT(head.UpdateDate, 'yyyyMMdd'), head.UpdateTS) AS serial
+                                        CONCAT('SO', head.DocNum, '-', FORMAT(head.UpdateDate, 'yyyyMMdd'), head.UpdateTS) AS serial
                                     FROM (
-                                    SELECT DocEntry, UpdateDate, UpdateTS
+                                    SELECT DocEntry, DocNum, UpdateDate, UpdateTS
                                     FROM ORDR WITH(NOLOCK)
                                     WHERE CAST(CreateDate AS date) = CAST(GETDATE() AS date)
                                     OR CAST(UpdateDate AS date) = CAST(GETDATE() AS date)
@@ -81,9 +82,9 @@ export class SAPEventListener {
                                     
                                     SELECT
                                         line.ItemCode AS id,
-                                        CONCAT('AR-', FORMAT(head.UpdateDate, 'yyyyMMdd'), head.UpdateTS) AS serial
+                                        CONCAT('AR', head.DocNum, '-', FORMAT(head.UpdateDate, 'yyyyMMdd'), head.UpdateTS) AS serial
                                     FROM (
-                                    SELECT DocEntry, UpdateDate, UpdateTS
+                                    SELECT DocEntry, DocNum, UpdateDate, UpdateTS
                                     FROM OINV WITH(NOLOCK)
                                     WHERE CAST(CreateDate AS date) = CAST(GETDATE() AS date)
                                     OR CAST(UpdateDate AS date) = CAST(GETDATE() AS date)
@@ -185,6 +186,173 @@ export class SAPEventListener {
                                         Y.DuplicateInMainTable = '' 
                                         OR Y.DuplicateInMainTable IS NULL
                                     )
+
+                                    UNION
+
+                                    SELECT * FROM (SELECT
+                                        SE.U_BookingNumber as id,
+                                        CONCAT('SBDI-', FORMAT(GETDATE(), 'yyyyMMddhhmmss')) AS serial
+                                    FROM SUMMARY_EXTRACT SE WITH(NOLOCK)
+                                    LEFT JOIN BILLING_EXTRACT BE ON BE.U_BookingId = SE.U_BookingNumber
+                                    WHERE ((
+                                        (BE.U_BillingStatus <> SE.U_BillingStatus AND REPLACE(BE.U_BillingStatus, ' ', '') <> REPLACE(SE.U_BillingStatus, ' ', '')) 
+                                        OR (
+                                            (
+                                                BE.U_BillingStatus IS NOT NULL AND REPLACE(BE.U_BillingStatus, ' ', '') <> '' 
+                                                AND (SE.U_BillingStatus IS NULL OR REPLACE(SE.U_BillingStatus, ' ', '') = '')
+                                            )
+                                            OR (
+                                                SE.U_BillingStatus IS NOT NULL AND REPLACE(SE.U_BillingStatus, ' ', '') <> '' 
+                                                AND (BE.U_BillingStatus IS NULL OR REPLACE(BE.U_BillingStatus, ' ', '') = '')
+                                            )
+                                        )
+                                    )
+                                    OR (
+                                        (BE.U_InvoiceNo <> SE.U_InvoiceNo AND REPLACE(BE.U_InvoiceNo, ' ', '') <> REPLACE(SE.U_InvoiceNo, ' ', '')) 
+                                        OR (
+                                            (
+                                                BE.U_InvoiceNo IS NOT NULL AND REPLACE(BE.U_InvoiceNo, ' ', '') <> '' 
+                                                AND (SE.U_InvoiceNo IS NULL OR REPLACE(SE.U_InvoiceNo, ' ', '') = '')
+                                            )
+                                            OR (
+                                                SE.U_InvoiceNo IS NOT NULL AND REPLACE(SE.U_InvoiceNo, ' ', '') <> '' 
+                                                AND (BE.U_InvoiceNo IS NULL OR REPLACE(BE.U_InvoiceNo, ' ', '') = '')
+                                            )
+                                        )
+                                    )
+                                    OR (
+                                        (BE.U_PODSONum <> SE.U_PODSONum AND REPLACE(BE.U_PODSONum, ' ', '') <> REPLACE(SE.U_PODSONum, ' ', '')) 
+                                        OR (
+                                            (
+                                                BE.U_PODSONum IS NOT NULL AND REPLACE(BE.U_PODSONum, ' ', '') <> '' 
+                                                AND (SE.U_PODSONum IS NULL OR REPLACE(SE.U_PODSONum, ' ', '') = '')
+                                            )
+                                            OR (
+                                                SE.U_PODSONum IS NOT NULL AND REPLACE(SE.U_PODSONum, ' ', '') <> '' 
+                                                AND (BE.U_PODSONum IS NULL OR REPLACE(BE.U_PODSONum, ' ', '') = '')
+                                            )
+                                        )
+                                    )
+                                    OR (
+                                        (BE.U_DocNum <> SE.U_ARDocNum AND REPLACE(BE.U_DocNum, ' ', '') <> REPLACE(SE.U_ARDocNum, ' ', '')) 
+                                        OR (
+                                            (
+                                                BE.U_DocNum IS NOT NULL AND REPLACE(BE.U_DocNum, ' ', '') <> '' 
+                                                AND (SE.U_ARDocNum IS NULL OR REPLACE(SE.U_ARDocNum, ' ', '') = '')
+                                            )
+                                            OR (
+                                                SE.U_ARDocNum IS NOT NULL AND REPLACE(SE.U_ARDocNum, ' ', '') <> '' 
+                                                AND (BE.U_DocNum IS NULL OR REPLACE(BE.U_DocNum, ' ', '') = '')
+                                            )
+                                        )
+                                    ))
+                                    AND BE.U_BookingId IS NOT NULL
+                                    
+                                    UNION
+                                    -----> issue #22
+                                    SELECT
+                                        T0.U_BookingNumber as id,
+                                        CONCAT('TBVNR-', FORMAT(GETDATE(), 'yyyyMMddhhmmss')) AS serial
+                                    FROM [dbo].[@PCTP_POD] T0 WITH(NOLOCK)
+                                    WHERE 1=1
+                                    AND (CAST(T0.U_PODStatusDetail as nvarchar(max)) LIKE '%Verified%' OR CAST(T0.U_PODStatusDetail as nvarchar(max)) LIKE '%ForAdvanceBilling%')
+                                    AND T0.U_BookingNumber NOT IN (SELECT U_BookingId FROM BILLING_EXTRACT WITH(NOLOCK))
+                                    UNION
+                                    SELECT
+                                        T0.U_BookingNumber as id,
+                                        CONCAT('TBVNR-', FORMAT(GETDATE(), 'yyyyMMddhhmmss')) AS serial
+                                    FROM [dbo].[@PCTP_POD] T0 WITH(NOLOCK)
+                                    WHERE 1=1
+                                    AND (CAST(T0.U_PODStatusDetail as nvarchar(max)) LIKE '%Verified%')
+                                    AND T0.U_BookingNumber NOT IN (SELECT U_BookingId FROM TP_EXTRACT WITH(NOLOCK))
+                                    
+                                    UNION
+                                    -----> issue #23
+                                    SELECT
+                                        BE.U_BookingId AS id,
+                                        CONCAT('BTPDI-', FORMAT(GETDATE(), 'yyyyMMddhhmmss')) AS serial
+                                    FROM BILLING_EXTRACT BE WITH(NOLOCK)
+                                    LEFT JOIN PRICING_EXTRACT PE ON PE.U_BookingId = BE.U_BookingId
+                                    WHERE (
+                                        TRY_PARSE(PE.U_GrossClientRatesTax AS FLOAT) <> TRY_PARSE(BE.U_GrossInitialRate AS FLOAT)
+                                        OR TRY_PARSE(PE.U_Demurrage AS FLOAT) <> TRY_PARSE(BE.U_Demurrage AS FLOAT)
+                                        OR TRY_PARSE(PE.U_TotalAddtlCharges AS FLOAT) <> TRY_PARSE(BE.U_AddCharges AS FLOAT)
+                                        OR ((
+                                                (TRY_PARSE(PE.U_GrossClientRatesTax AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_GrossClientRatesTax AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(BE.U_GrossInitialRate AS FLOAT) IS NULL OR TRY_PARSE(BE.U_GrossInitialRate AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_Demurrage AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_Demurrage AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(BE.U_Demurrage AS FLOAT) IS NULL OR TRY_PARSE(BE.U_Demurrage AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_TotalAddtlCharges AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_TotalAddtlCharges AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(BE.U_AddCharges AS FLOAT) IS NULL OR TRY_PARSE(BE.U_AddCharges AS FLOAT) = 0)
+                                        ))
+                                    )
+                                    UNION
+                                    SELECT
+                                        TE.U_BookingId AS id,
+                                        CONCAT('BTPDI-', FORMAT(GETDATE(), 'yyyyMMddhhmmss')) AS serial
+                                    FROM TP_EXTRACT TE WITH(NOLOCK)
+                                    LEFT JOIN PRICING_EXTRACT PE ON PE.U_BookingId = TE.U_BookingId
+                                    WHERE (
+                                        TRY_PARSE(PE.U_GrossTruckerRates AS FLOAT) <> TRY_PARSE(TE.U_GrossTruckerRates AS FLOAT)
+                                        OR TRY_PARSE(PE.U_GrossTruckerRatesTax AS FLOAT) <> TRY_PARSE(TE.U_GrossTruckerRatesN AS FLOAT)
+                                        OR TRY_PARSE(PE.U_RateBasisT AS FLOAT) <> TRY_PARSE(TE.U_RateBasis AS FLOAT)
+                                        OR TRY_PARSE(PE.U_Demurrage2 AS FLOAT) <> TRY_PARSE(TE.U_Demurrage AS FLOAT)
+                                        OR TRY_PARSE(PE.U_AddtlDrop2 AS FLOAT) <> TRY_PARSE(TE.U_AddtlDrop AS FLOAT)
+                                        OR TRY_PARSE(PE.U_BoomTruck2 AS FLOAT) <> TRY_PARSE(TE.U_BoomTruck AS FLOAT)
+                                        OR TRY_PARSE(PE.U_Manpower2 AS FLOAT) <> TRY_PARSE(TE.U_Manpower AS FLOAT)
+                                        OR TRY_PARSE(PE.U_Backload2 AS FLOAT) <> TRY_PARSE(TE.U_BackLoad AS FLOAT)
+                                        OR TRY_PARSE(PE.U_totalAddtlCharges2 AS FLOAT) <> TRY_PARSE(TE.U_Addtlcharges AS FLOAT)
+                                        OR TRY_PARSE(PE.U_Demurrage3 AS FLOAT) <> TRY_PARSE(TE.U_DemurrageN AS FLOAT)
+                                        OR TRY_PARSE(PE.U_AddtlCharges AS FLOAT) <> TRY_PARSE(TE.U_AddtlChargesN AS FLOAT)
+                                        OR ((
+                                                (TRY_PARSE(PE.U_GrossTruckerRates AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_GrossTruckerRates AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(TE.U_GrossTruckerRates AS FLOAT) IS NULL OR TRY_PARSE(TE.U_GrossTruckerRates AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_GrossTruckerRatesTax AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_GrossTruckerRatesTax AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(TE.U_GrossTruckerRatesN AS FLOAT) IS NULL OR TRY_PARSE(TE.U_GrossTruckerRatesN AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_RateBasisT AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_RateBasisT AS FLOAT) <> '')
+                                                AND (TRY_PARSE(TE.U_RateBasis AS FLOAT) IS NULL OR TRY_PARSE(TE.U_RateBasis AS FLOAT) = '')
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_Demurrage2 AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_Demurrage2 AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(TE.U_Demurrage AS FLOAT) IS NULL OR TRY_PARSE(TE.U_Demurrage AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_AddtlDrop2 AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_AddtlDrop2 AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(TE.U_AddtlDrop AS FLOAT) IS NULL OR TRY_PARSE(TE.U_AddtlDrop AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_BoomTruck2 AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_BoomTruck2 AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(TE.U_BoomTruck AS FLOAT) IS NULL OR TRY_PARSE(TE.U_BoomTruck AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_Manpower2 AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_Manpower2 AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(TE.U_Manpower AS FLOAT) IS NULL OR TRY_PARSE(TE.U_Manpower AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_Backload2 AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_Backload2 AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(TE.U_BackLoad AS FLOAT) IS NULL OR TRY_PARSE(TE.U_BackLoad AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_totalAddtlCharges2 AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_totalAddtlCharges2 AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(TE.U_Addtlcharges AS FLOAT) IS NULL OR TRY_PARSE(TE.U_Addtlcharges AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_Demurrage3 AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_Demurrage3 AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(TE.U_DemurrageN AS FLOAT) IS NULL OR TRY_PARSE(TE.U_DemurrageN AS FLOAT) = 0)
+                                            )
+                                            OR (
+                                                (TRY_PARSE(PE.U_AddtlCharges AS FLOAT) IS NOT NULL AND TRY_PARSE(PE.U_AddtlCharges AS FLOAT) <> 0)
+                                                AND (TRY_PARSE(TE.U_AddtlChargesN AS FLOAT) IS NULL OR TRY_PARSE(TE.U_AddtlChargesN AS FLOAT) = 0)
+                                        ))
+                                    )) X WHERE X.id IS NOT NULL
                                 `, async (err: Error | undefined, recordset: sql.IResult<unknown> | undefined) => {
                                     if (err) {
                                         console.log(err)
